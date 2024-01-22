0771dd152769cc5a5a1f4d680cd770e7c8a4ea83
Luke Trenaman <trenamanluke@gmail.com>
2024-01-22 12:45:27 -0500

    refactor animations

--- components/AnimatedHeader.tsx
+++ components/AnimatedHeader.tsx
@@ -1,6 +1,67 @@
 import { motion } from "framer-motion";
+import { Variants } from "framer-motion";
 
+const horizontal = {
+  initial:{
+    width:0,
+    height: "var(--corner-size)",
+  },
+  animate:{
+    width: "var(--corner-length)",
+  },
+};
+const vertical = {
+  initial:{
+    width:"var(--corner-size)",
+    height: 0,
+  },
+  animate:{
+    height: "var(--corner-length)",
+  },
+};
+const topLeftVariants = {
+  initial: {
+    top:"var(--corner-start)",
+    left:"var(--corner-start)",
+  },
+  animate: {
+    top:"var(--corner-end)",
+    left:"var(--corner-end)",
+  },
+};
+const bottomRightVariants = {
+  initial: {
+    bottom:"-200px",
+    right:"-200px",
+  },
+  animate: {
+    bottom:"var(--corner-end)",
+    right:"var(--corner-end)",
+  },
+};
+const combine = (a: Variants, b: Variants): Variants => {
+  return {
+    initial:{
+      ...a.initial,
+      ...b.initial,
+    },
+    animate:{
+      ...a.animate,
+      ...b.animate,
+    }
+  };
+};
 export function AnimatedHeader(){
+  // variantArr contains objects with each combination between horizontal/vertical and topLeftVariants / bottomRightVariants
+  // each one has an initial and animate nested object, account for this
+
+  const variantArr = [
+    combine(horizontal, topLeftVariants),
+    combine(vertical, topLeftVariants),
+    combine(horizontal, bottomRightVariants),
+    combine(vertical, bottomRightVariants),
+  ];
+  console.log(variantArr);
   return (
     <header className="my-52 mx-auto">
       <div className="text-center text-lg text-textAccent">
@@ -12,30 +73,14 @@ export function AnimatedHeader(){
         transition={{ duration: 1.5 }}
         className="text-center">
         <span className="relative text-textPrimary text-6xl font-bold">
-          <motion.div
-            className="absolute bg-[#CEF6C6]"
-            initial={{ width:0, height: 4, top:-200, left:-200 }}
-            animate={{ width: 35, top:-50, left: -50 }}
-            transition={{ duration: 0.7, delay:1.5 }}
-          />
-          <motion.div
-            className="absolute bg-[#CEF6C6]"
-            initial={{ width:4, height: 0, top:-200, left:-200 }}
-            animate={{ height: 35, top:-50, left:-50 }}
-            transition={{ duration: 0.7, delay:1.5 }}
-          />
-          <motion.div
-            className="absolute bg-[#CEF6C6]"
-            initial={{ width:0, height: 4, bottom:-200, right:-200 }}
-            animate={{ width: 35, bottom: -50, right: -50 }}
-            transition={{ duration: 0.7, delay:1.5 }}
-          />
-          <motion.div
-            className="absolute bg-[#CEF6C6]"
-            initial={{ width:4, height: 0, bottom:-200, right:-200 }}
-            animate={{ height: 35, bottom: -50, right: -50 }}
-            transition={{ duration: 0.7, delay:1.5 }}
-          />
+          {variantArr.map((variant,i) => <motion.div 
+            key={i}
+            className="absolute bg-textAccent"
+            initial="initial"
+            animate="animate"
+            transition={{ duration:20, delay:1.5 }}
+            variants={variant}
+          />)}
           Luke Trenaman
         </span>
       </motion.div>
