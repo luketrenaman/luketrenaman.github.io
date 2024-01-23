68084ba9b67247dab6d48f5cd2c6373d779deead
Luke Trenaman <trenamanluke@gmail.com>
2024-01-22 21:55:48 -0500

    postfix with index.html

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  rewrites: async () => {
-    return [
-      {
-        source: '/',
-        destination: '/index.html',
-      },
-    ]
-},
 }
 
 module.exports = nextConfig
