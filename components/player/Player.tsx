5c12a3b473708eb1f6f51658a83ea4474b33aa82
Luke Trenaman <trenamanluke@gmail.com>
2024-02-26 16:45:43 -0500

    try adding fixed position to iframes

--- components/player/Player.tsx
+++ components/player/Player.tsx
@@ -2,5 +2,5 @@ export interface PlayerProps{
   src: string;
 }
 export function Player({ src } : PlayerProps){
-  return <iframe className="block bg-white border-0 h-[100vh] w-[100vw]" src={src}></iframe>;
+  return <iframe className="fixed block bg-white border-0 h-[100vh] w-[100vw]" src={src}></iframe>;
 }
