e20885aaa6b4937dbc2ccf1c933c36772a4f1dee
Luke Trenaman <trenamanluke@gmail.com>
2024-02-26 15:14:14 -0500

    feat: redirects to code site based on old url paths

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
+function makeRedirect(source){
+  return {source, destination: `https://code.luketrenaman.com/${source}`, permanent: true};
+}
 const nextConfig = {
   reactStrictMode: true,
+  async redirects(){
+    return [
+      makeRedirect("/archives/2d"),
+      makeRedirect("/archives/conway"),
+      makeRedirect("/archives/music"),
+      makeRedirect("/archives/space_glider"),
+      makeRedirect("/archives/tandem"),
+      makeRedirect("/archives/ukulele"),
+      makeRedirect("/ducksouls"),
+      makeRedirect("/luketris"),
+      makeRedirect("/rotator"),
+      makeRedirect("/snakemaze"),
+      makeRedirect("/wave_editor"),
+    ]
+  }
 }
 
 module.exports = nextConfig
