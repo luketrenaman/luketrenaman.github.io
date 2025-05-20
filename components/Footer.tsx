06f10e662499fc1a8b491e04e9b6d7a423c5fb5c
Luke Trenaman <trenamanluke@gmail.com>
2025-05-20 15:46:43 -0400

    Switch soundcloud to bandcamp and put it before stackoverflow

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,8 +4,8 @@ export function Footer(){
       <a href="https://github.com/luketrenaman" title="GitHub"><i className="fa-brands fa-github"></i></a>
       <a href="https://www.linkedin.com/in/luke-trenaman-8854a11a3/" title="LinkedIn"><i className="fa-brands fa-linkedin"></i></a>
       <a href="https://luketrenaman.itch.io/" title="itch.io"><i className="fa-brands fa-itch-io"></i></a>
+      <a href="https://luketrenaman.bandcamp.com/" title="BandCamp"><i className="fa-brands fa-bandcamp"></i></a>
       <a href="https://stackoverflow.com/users/17460969/luke-trenaman" title="StackOverflow"><i className="fa-brands fa-stack-overflow"></i></a>
-      <a href="https://soundcloud.com/ltrenaman" title="SoundCloud"><i className="fa-brands fa-soundcloud"></i></a>
     </footer>
   );
 }
